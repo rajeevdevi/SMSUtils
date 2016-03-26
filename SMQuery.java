703793e1b88f3062a57fe7ec433c44cea2c9ad38
Pradeep Subrahmanian <rajeevdevi9@gmail.com>
2016-03-26 09:37:55 +0530

    my commit

---
+++ SMQuery.java
@@ -0,0 +1,41 @@
+package com.appdvaya.smsapp.myapplication;
+
+
+public class SMQuery {
+
+
+    private String message;
+    private String phoneNumber;
+    private String contactName;
+    private String date;
+
+    public String getMessageToSearch() {
+
+        return message;
+    }
+
+    public void setMessageToSearch(String message) {
+
+        this.message = message;
+    }
+
+    public String getPhoneNumberToSearch() {
+
+        return phoneNumber;
+    }
+
+
+    public void setNumberToSearch(String phoneNumber) {
+
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getContactNameToSearch() {
+        return contactName;
+    }
+
+    public void setContactNameToSearch(String contactName) {
+        this.contactName = contactName;
+    }
+
+}
